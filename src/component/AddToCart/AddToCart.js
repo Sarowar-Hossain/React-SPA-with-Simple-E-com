bacb6705e5a0c2ca270e731acee7799aed33078f
Sarowar-Hossain <mdsarowar1812@gmail.com>
2023-03-27 12:30:14 +0800

    bug solve

--- src/component/AddToCart/AddToCart.js
+++ src/component/AddToCart/AddToCart.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+  import { faArrowCircleRight, faCoffee, faDeleteLeft, faRemove } from '@fortawesome/free-solid-svg-icons'
 import './AddToCart.css'
 
 const AddToCart = () => {
@@ -10,6 +12,12 @@ const AddToCart = () => {
             <p className='p-tag'>Tax: $</p>
             <div className='equal-line'></div>
             <p className='total-price'>Grand Total: $</p>
+            <button className='clear-cart-btn'>Clear Cart
+            <FontAwesomeIcon icon={faRemove} className="highlight" />
+            </button>
+            <button className='review-btn'>Review Order 
+            <FontAwesomeIcon icon={faArrowCircleRight} className="highlight"/>
+            </button>
         </div>
     );
 };
