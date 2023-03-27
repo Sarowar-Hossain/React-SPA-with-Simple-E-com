e6c3654de5f5be53efe55d07a904ed2e62c1e1e4
Sarowar-Hossain <mdsarowar1812@gmail.com>
2023-03-27 11:13:48 +0800

    bug flxed

--- src/component/Products/Products.js
+++ src/component/Products/Products.js
@@ -1,21 +1,32 @@
-import React, { useEffect, useState } from 'react';
-import Product from '../Product/Product';
-import './Products.css'
+import React, { useEffect, useState } from "react";
+import "./Products.css";
 
 const Products = () => {
-    const [products, setProducts]= useState([])
-    useEffect(()=>{
-        fetch('https://raw.githubusercontent.com/ProgrammingHero1/ema-john-resources/main/fakeData/products.json')
-        .then(res=>res.json())
-        .then(data=>setProducts(data))
-    },[])
-    console.log(products)
-    return (
-        <div className='container'>
-            <Product Products={products}></Product>
-            
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    fetch(
+      "https://raw.githubusercontent.com/ProgrammingHero1/ema-john-resources/main/fakeData/products.json"
+    )
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
+  console.log(products);
+  return (
+    <div className="container">
+      {products.map((product) => (
+        <div className="product-img" key={product.id}>
+          <img src={product.img} alt="" />
+          <div className="product-details">
+            <h4>{product.name}</h4>
+            <p>Price:{product.price}$</p>
+            <p>Manufacturer: {product.seller}</p>
+            <p>Ratings: {product.ratings} star </p>
+            <button>Add to Cart</button>
+          </div>
         </div>
-    );
+      ))}
+    </div>
+  );
 };
 
 export default Products;
