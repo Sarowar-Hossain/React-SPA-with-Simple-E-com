5aeb8e35323f2e536d86467b3837fec93500e02a
Sarowar-Hossain <mdsarowar1812@gmail.com>
2023-03-28 00:18:58 +0800

    cart calculation done

--- src/component/AddToCart/AddToCart.js
+++ src/component/AddToCart/AddToCart.js
@@ -3,16 +3,26 @@ import { faArrowCircleRight, faRemove} from "@fortawesome/free-solid-svg-icons";
 import "./AddToCart.css";
 
 const AddToCart = ({ items }) => {
-  
+  console.log(items);
+  let totalPrice = 0;
+  let totalShipping = 0;
+
+  for (const item of items) {
+    totalPrice = totalPrice + item.price;
+    totalShipping = totalShipping + item.shipping;
+  }
+  const tax = totalPrice*7/100;
+  const total = totalPrice + totalShipping + tax;
+  console.log(totalPrice);
   return (
     <div className="order-details">
       <h4>Order Summary</h4>
       <p>Selected Items: {items.length}</p>
-      <p>Total Price: $</p>
-      <p>Total Shipping Charge: $</p>
-      <p>Tax: $</p>
+      <p>Total Price: $ {totalPrice}</p>
+      <p>Total Shipping Charge: $ {totalShipping}</p>
+      <p>Tax: $ {tax.toFixed(2)}</p>
       <div className="equal-line"></div>
-      <h5 className="total-price">Grand Total: $</h5>
+      <h5 className="total-price">Grand Total: $ {total.toFixed(2)}</h5>
 
       <button className="clear-cart-btn">
         Clear Cart
